9e37b5731ac297e8da283a6aac6699285157ad7b
SChau <71276205+SamsonChau@users.noreply.github.com>
2022-02-15 23:49:57 +0800

    Add files via upload

--- vesc/main.cpp
+++ vesc/main.cpp
@@ -1,7 +1,8 @@
 #include "mbed.h"
 #include "vesc.h"
-#define TARGET_TX_PIN                                                     USBTX
-#define TARGET_RX_PIN                                                     USBRX
+
+#define TARGET_TX_PIN USBTX
+#define TARGET_RX_PIN USBRX
 static BufferedSerial serial_port(TARGET_TX_PIN, TARGET_RX_PIN, 115200);
 /*
 config the vesc in vesc tools before testing
@@ -10,48 +11,48 @@ the read status function not tested yet
 */
 
 // set the id and can bus baud rate of the VESC
-int can_baud = 500000;
+int can_baud = 1000000;
 int can_id = 90;
 
-Thread vesc_thread;
-Thread print_thread;
-Thread control_thread;
+// CAN can1(PA_11, PA_12, can_baud); // define the can bus object & vesc object
+CAN can(PB_5, PB_6, can_baud);    
 
-CAN can1(PA_11, PA_12, can_baud); // define the can bus object & vesc object
+Thread vesc_thread(osPriorityNormal);
+Thread control_thread(osPriorityLow);
+
+EventQueue printfQueue;
+EventQueue readQueue;
+
+Ticker ThreadTicker;
+Ticker printreadTicker;
+                                                                                                                                                                                                                                                                                                                                
 vesc _vesc1;
 
-void vesc_th() {
-  _vesc1.vesc_init(&can1, can_baud);
-  _vesc1.set_monitor_id(can_id); // This enable the can bus monitor particular
-                                 // id, may improve later
-  while (true) {
-    _vesc1.can_read();
-    printf("Read one msgs\r\n");
-    ThisThread::sleep_for(10);
-  }
-}
+void vesc_th() { _vesc1.can_read(can_id); }
+
 void debug_msgs() {
-  while (true) {
-    // reading value
-    printf("position: %d ", _vesc1.read_pos(can_id));
-    printf("rpm: %d ", _vesc1.read_rpm(can_id));
-    printf("current: %d ", _vesc1.read_current(can_id));
-    printf("ESC current: %d ", _vesc1.read_esc_current(can_id));
-    printf("input_voltage: %d ", _vesc1.read_input_voltage(can_id));
-    printf("FET temp: %d \r\n", _vesc1.read_fet_temp(can_id));
-    ThisThread::sleep_for(500);
-  }
+  // reading value
+  printf("position: %.2f ", _vesc1.read_pos(can_id));
+  printf("rpm: %.2f ", _vesc1.read_rpm(can_id));
+  printf("current: %.2f ", _vesc1.read_current(can_id));
+  printf("ESC current: %.2f ", _vesc1.read_esc_current(can_id));
+  printf("input_voltage: %.2f ", _vesc1.read_input_voltage(can_id));
+  printf("FET temp: %.2f \r\n", _vesc1.read_fet_temp(can_id));
 }
-void control_cmd(){
-    while(true){
-        //_vesc1.set_curren(can_id, 3.0);
-        printf("Send one msgs\r\n");
-        ThisThread::sleep_for(100);
-    }
+void control_cmd() {
+  //_vesc1.set_rpm(can_id, 10000);
+  //printf("Send one msgs\r\n");
+  printfQueue.call(&debug_msgs);
 }
 int main() {
+  _vesc1.vesc_init(&can, can_baud);
+  _vesc1.set_monitor_id(can_id); // This enable the can bus monitor particular
+                                 // id, may improve later
+
   // initslize the vesc
-  vesc_thread.start(vesc_th);
-  print_thread.start(debug_msgs);
-  control_thread.start(control_cmd);
+  vesc_thread.start(callback(&readQueue, &EventQueue::dispatch_forever));
+  control_thread.start(callback(&printfQueue, &EventQueue::dispatch_forever));
+  
+  printreadTicker.attach(readQueue.event(&vesc_th), 0.001);
+  ThreadTicker.attach(printfQueue.event(&control_cmd), 0.05);
 }
--- vesc/vesc/vesc.h
+++ vesc/vesc/vesc.h
@@ -8,34 +8,34 @@ class vesc
 {
 public:
     void vesc_init(CAN* _CAN, int baud);                        //init the object  
-    void set_monitor_id(uint8_t id);                            //set the read status vesc id, currently only support one only
+    void set_monitor_id(int id);                            //set the read status vesc id, currently only support one only
 
-    void set_duty(uint8_t id,float duty);                       //set the duty cycle set point for the motor
-    void set_current(uint8_t id, float current);                //set the current set point for the motor
-    void set_current_brake(uint8_t id, float current);          //set the braking current of the motor
-    void set_rpm(uint8_t id, float rpm);                        //set the velocity of the motor
-    void set_pos(uint8_t id, float pos);                        //set the position of the motor
+    void set_duty(int id,float duty);                       //set the duty cycle set point for the motor
+    void set_current(int id, float current);                //set the current set point for the motor
+    void set_current_brake(int id, float current);          //set the braking current of the motor
+    void set_rpm(int id, float rpm);                        //set the velocity of the motor
+    void set_pos(int id, float pos);                        //set the position of the motor
 
-    int16_t read_pos(uint8_t id);                               //read the pid position feedback 
-    int32_t read_rpm(uint8_t id);                               //read the erpm of the motor
-    int16_t read_current(uint8_t id);                           //read the current to the motor
-    int16_t read_esc_current(uint8_t id);                       //read the input bus current
-    int16_t read_input_voltage(uint8_t id);                     //read the input voltage of the bus
-    int16_t read_fet_temp(uint8_t id);                          //read the motor tempeture of the FET
-    void can_read();
+    float read_pos(int id);                               //read the pid position feedback 
+    float read_rpm(int id);                               //read the erpm of the motor
+    float read_current(int id);                           //read the current to the motor
+    float read_esc_current(int id);                       //read the input bus current
+    float read_input_voltage(int id);                     //read the input voltage of the bus
+    float read_fet_temp(int id);                          //read the motor tempeture of the FET
+    void can_read(int id);
 
 private:
     void package_msg(uint8_t* buffer, int32_t number, int32_t *index);
-    bool can_send(uint32_t id, uint8_t packet[], int32_t len);
+    bool can_send(int id, uint8_t packet[], int32_t len);
     void reset_param();
 
-    uint8_t current_id;
-    int16_t pid_pos;
-    int32_t rpm;
-    int16_t current;
-    int16_t esc_current;
-    int16_t input_voltage;
-    int16_t fet_temp;
+    int current_id;
+    float pid_pos;
+    float rpm;
+    float current;
+    float esc_current;
+    float input_voltage;
+    float fet_temp;
 
     CAN* can;
     CANMessage Txmsg;
