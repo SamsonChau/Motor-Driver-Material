ce80acbabf09ecc85f4d7627c50c9ceed70fed25
SChau <71276205+SamsonChau@users.noreply.github.com>
2022-02-23 11:04:50 +0800

    Update main.cpp

--- vesc/main.cpp
+++ vesc/main.cpp
@@ -1,21 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////
+/*
+    VESC libraray sample program, 
+    Use mbed os 6.13 to compile.
+    connect the vesc with the STM32 via can bus
+    config the vesc in vesc tools before connect to the VESC
+    change the setting according your need
+    
+    ALL cmd are usingg the same unit as vesc
+*/
+/////////////////////////////////////////////////////////////////////////////
 #include "mbed.h"
 #include "vesc.h"
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+      USB serial config 
+      for show the debug msg and display the 
+*/
+////////////////////////////////////////////////////////////////////////////
 
 #define TARGET_TX_PIN USBTX
 #define TARGET_RX_PIN USBRX
 static BufferedSerial serial_port(TARGET_TX_PIN, TARGET_RX_PIN, 115200);
-/*
-config the vesc in vesc tools before testing
-change the setting according your need
-the read status function not tested yet
-*/
 
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+      CAN BUS config 
+      set up the VESC can bus communication channel of the can bus 
+      Create one object for one VESC ID
+      if the read status is not work you may concider to slower the baudrate 
+      and the send status msgs frequency of the vesc
+      for one channel you can connect up to 10 VSEC or more(not tested more than 10)
+      
+      Hardware controller: Nucleo STM32F446RE
+      CAN-BUS pinout: can1 (PA_11, PA_12), can2 (PB_5, PB_6) 
+      example id: 90
+      example can bus baud rate: 1Mbps (MAX)
+      example vesc send status msgs frequency: 3000Hz (MAX) 
+*/
+////////////////////////////////////////////////////////////////////////////
 // set the id and can bus baud rate of the VESC
 int can_baud = 1000000;
 int can_id = 90;
 
-// CAN can1(PA_11, PA_12, can_baud); // define the can bus object & vesc object
+// define the can bus object
+// CAN can1(PA_11, PA_12, can_baud);  (choose one)
 CAN can(PB_5, PB_6, can_baud);    
+
+vesc _vesc1;
 
 Thread vesc_thread(osPriorityNormal);
 Thread control_thread(osPriorityLow);
@@ -26,7 +59,7 @@ EventQueue readQueue;
 Ticker ThreadTicker;
 Ticker printreadTicker;
                                                                                                                                                                                                                                                                                                                                 
-vesc _vesc1;
+
 
 void vesc_th() { _vesc1.can_read(can_id); }
 
