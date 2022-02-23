c23cb77d74ecac82470e999d3211d508333cb98e
SChau <71276205+SamsonChau@users.noreply.github.com>
2022-02-23 10:46:01 +0800

    Update vesc.cpp

--- vesc/vesc/vesc.cpp
+++ vesc/vesc/vesc.cpp
@@ -1,26 +1,26 @@
 #include "vesc.h"
 #include "mbed.h"
-
+//intialize the class vesc, can bus setting the can bus message type
 void vesc::vesc_init(CAN *_CAN, int baud) {
   can = _CAN;
   can->frequency(baud);
   Rxmsg.format = CANExtended;
 }
-
+//set up the which id you wanna monitor to
 void vesc::set_monitor_id(int id) {
   current_id = id;
   if (current_id != id) {
     reset_param();
   }
 }
-
+//internal function to format the data in to canbus msg
 void vesc::package_msg(uint8_t *buffer, int32_t number, int32_t *index) {
   buffer[(*index)++] = number >> 24;
   buffer[(*index)++] = number >> 16;
   buffer[(*index)++] = number >> 8;
   buffer[(*index)++] = number;
 }
-
+//Send the msg to the canbus from the buffer packet
 bool vesc::can_send(int id, uint8_t packet[], int32_t len) {
   Txmsg.id = id;
   Txmsg.format = CANExtended;
@@ -33,7 +33,7 @@ bool vesc::can_send(int id, uint8_t packet[], int32_t len) {
   can->write(Txmsg);
   return true;
 }
-
+//reset the parameter of the internal class  
 void vesc::reset_param() {
   current_id = 0;
   pid_pos = 0;
@@ -45,6 +45,11 @@ void vesc::reset_param() {
 }
 
 // Set vesc command
+
+// duty cycle setup cmd 
+// set up the duty cycle of the vec in %, simular to the PWM control 
+// same as the vesc tools
+// value -100 -> 0 -> 100
 void vesc::set_duty(int id, float duty) {
   int32_t send_index = 0;
   uint8_t buffer[4];
@@ -52,6 +57,10 @@ void vesc::set_duty(int id, float duty) {
   can_send(id | ((uint32_t)CAN_PACKET_SET_DUTY << 8), buffer, send_index);
 }
 
+// current control cmd
+// set up the drive current of the vesc in A
+// same as the vesc tools
+// value -60 0 -> 60 (physical limit, may vary from different current setting)
 void vesc::set_current(int id, float current) {
   int32_t send_index = 0;
   uint8_t buffer[4];
@@ -59,6 +68,10 @@ void vesc::set_current(int id, float current) {
   can_send(id | ((uint32_t)CAN_PACKET_SET_CURRENT << 8), buffer, send_index);
 }
 
+// braking current cmd 
+// set up the current for braking in A
+// same as vesc tool
+// value 0 (if you didnot config it)
 void vesc::set_current_brake(int id, float current) {
   int32_t send_index = 0;
   uint8_t buffer[4];
@@ -67,6 +80,13 @@ void vesc::set_current_brake(int id, float current) {
            send_index);
 }
 
+// velocity control cmd 
+// set up the velocity pid control set point in rpm/erpm
+// same as vesc tools 
+// for Sersorless motor the rpm means the erpm of the motor
+// for Hall Sensor/ Encoder motor this means the rpm read form the encoder
+// VESC6 (HW) erpm limit +-150000 
+// VESC4 (HW) erpm limit +-60000
 void vesc::set_rpm(int id, float rpm) {
   int32_t send_index = 0;
   uint8_t buffer[4];
@@ -74,6 +94,11 @@ void vesc::set_rpm(int id, float rpm) {
   can_send(id | ((uint32_t)CAN_PACKET_SET_RPM << 8), buffer, send_index);
 }
 
+// position control cmd 
+// set up the position pid control set point in deg
+// same as vesc tools 
+// for Sersorless motor not recommand to use this function
+// for Hall Sensor/ Encoder motor this means the position read form the encoder
 void vesc::set_pos(int id, float pos) {
   int32_t send_index = 0;
   uint8_t buffer[4];
@@ -82,6 +107,8 @@ void vesc::set_pos(int id, float pos) {
 }
 
 // read vesc command
+// decode the vesc can bus command 
+// reference to the xlsx file given
 void vesc::can_read(int id) {
   can->read(Rxmsg);
   int can_id = Rxmsg.id & 0xFF;
@@ -120,6 +147,8 @@ void vesc::can_read(int id) {
   }
 }
 
+// the position (in deg) provide by the vesc in deg
+// will return a float32 
 float vesc::read_pos(int id) {
   if (id != current_id) {
     current_id = id;
@@ -130,6 +159,8 @@ float vesc::read_pos(int id) {
     return pid_pos;
   }
 }
+// the rpm provide by the vesc in rpm
+// will return a float32 
 float vesc::read_rpm(int id) {
   if (id != current_id) {
     current_id = id;
@@ -140,6 +171,8 @@ float vesc::read_rpm(int id) {
     return rpm;
   }
 }
+// output current provide by the vesc in A
+// will return a float32 
 float vesc::read_current(int id) {
   if (id != current_id) {
     current_id = id;
@@ -150,6 +183,8 @@ float vesc::read_current(int id) {
     return esc_current;
   }
 }
+// the input bus current provide by the vesc in A
+// will return a float32 
 float vesc::read_esc_current(int id) {
   if (id != current_id) {
     current_id = id;
@@ -160,6 +195,8 @@ float vesc::read_esc_current(int id) {
     return esc_current;
   }
 }
+// the input bus voltage provide by the vesc in A
+// will return a float32 
 float vesc::read_input_voltage(int id) {
   if (id != current_id) {
     current_id = id;
@@ -170,7 +207,8 @@ float vesc::read_input_voltage(int id) {
     return input_voltage;
   }
 }
-
+// the Mosfet temperture provided by vesc in deg (C)
+// will return a float32 
 float vesc::read_fet_temp(int id) {
   if (id != current_id) {
     current_id = id;
